d95004eee70f2fcb17002c462f7a5252cdcd3c8a
Phant0m The Great <120599821+Phant0mthegreat@users.noreply.github.com>
2023-06-22 08:09:26 -0300

    Update banners.py

--- banners.py
+++ banners.py
@@ -1,35 +1,22 @@
 from pystyle import Colorate, Colors
-
 import banners, os, time
 
 banner1="""      ██╗  ██╗██████╗ ██╗██████╗ ████████╗
-
       ██║ ██╔╝██╔══██╗██║██╔══██╗╚══██╔══╝
-
       █████╔╝ ██████╔╝██║██████╔╝   ██║   
-
       ██╔═██╗ ██╔══██╗██║██╔═══╝    ██║   
-
       ██║  ██╗██║  ██║██║██║        ██║   
-
       ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝        ╚═╝   
-
                                """
-
 def carregando():
-
   os.system('clear')
-
   print(Colorate.Vertical(Colors.red_to_yellow, '''
 
+
+
            ╔═════════════════════╗
-
            ║        Kript        ║
-
            ║     Carregando...   ║
-
            ╚═════════════════════╝
-
                                                                '''))
-
   time.sleep(3)
